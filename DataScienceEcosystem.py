9ca47979b3c5001f05a33cf88e23dcff92963f8c
RitaRGomes <95661129+RitaRGomes@users.noreply.github.com>
2023-08-09 15:01:37 +0100

    Final Lab

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> Data Science Tools and Ecosystem </h1>
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+#  <ol>
+#   <li>Python</li>
+#   <li>R</li>
+#   <li>SQL</li>
+#   <li>Java</li>
+#    <li>Julia</li>
+# </ol> 
+
+# Some of the commonly used libraries used by Data Scientists include:
+#  <ol>
+#   <li>Pandas</li>
+#   <li>NumPy</li>
+#   <li>Matplotlib</li>
+#   <li>Scikit-learn</li>
+# </ol>
+
+# <table>
+#   <tr>
+#       <th>Data Science Tools</th>
+#   </tr>
+#   <tr>
+#       <td>Jupyter</td> 
+#   </tr>
+#   <tr>
+#       <td>Apache Zeppelin</td> 
+#   </tr>
+#   <tr>
+#       <td>RStudio</td> 
+#   </tr>
+# </table>
+
+# <h3>Below are a few examples of evaluating arithmetic expressions in Python </h3>
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5 
+
+
+# In[7]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# <b> Objectives:</b>
+# <ul>
+#     <li>List popular languages for Data Science</li>
+#     <li>List commonly used libraries by Data Scientists</li>
+#     <li>Create a table with open source Data Science Tools</li>
+#     <li>Create a simple arithmetic expression to mutiply then add integers</li>
+#     <li>Create a converter of minutes to hours by diving them by 60</li>
+# </ul>
+
+# <h2> Author </h2>
+# Rita Gomes
+# 
+# 
+
+# In[ ]:
+
+
+
+
